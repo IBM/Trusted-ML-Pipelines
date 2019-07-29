11395ca6f2e94ce771597e1816370477cf0b455b
dpnichols <dnichols@us.ibm.com>
2019-07-29 10:08:07 -0500

    Add files via upload

--- conf/JSM2019/R Files/GRU.R
+++ conf/JSM2019/R Files/GRU.R
@@ -16,7 +16,6 @@ library(keras)
 
 max_features <- 10000
 maxlen <- 500
-batch_size <- 32
 
 imdb <- dataset_imdb(num_words = max_features)
 c(c(input_train, y_train), c(input_test, y_test)) %<-% imdb 
